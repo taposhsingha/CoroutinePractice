b48b12388ba69eeaf65dbc6f54542c1e36ea4e6d
taposhsingha <57071916+taposhsingha@users.noreply.github.com>
2023-02-09 19:21:52 +0600

    basic coroutine example codes added

---
+++ app/src/main/java/com/example/coroutinepractice/MainActivity.kt
@@ -0,0 +1,42 @@
+package com.example.coroutinepractice
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.TextView
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+class MainActivity : AppCompatActivity() {
+    private var count = 0
+    private lateinit var messageTextView: TextView
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val textView = findViewById<TextView>(R.id.tvCount)
+        messageTextView = findViewById(R.id.tvMessage)
+        val countButton = findViewById<Button>(R.id.btnCount)
+        val downloadButton = findViewById<Button>(R.id.btnDownload)
+
+        countButton.setOnClickListener {
+            textView.text = count++.toString()
+        }
+        downloadButton.setOnClickListener {
+            CoroutineScope(Dispatchers.IO).launch {
+                downloadUserData()
+            }
+        }
+    }
+    private suspend fun downloadUserData() {
+        for (i in 1..200000) {
+            Log.i("MyTag", "Downloading user $i in ${Thread.currentThread().name}")
+            //messageTextView.text = "Downloading user $i"
+            delay(100)
+        }
+    }
+}
+
